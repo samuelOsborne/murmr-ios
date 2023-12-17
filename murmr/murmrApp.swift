5477bdc5cb824a6e73493ad3fdde5d0818e76338
samuelOsborne <samu.osborne@gmail.com>
2023-12-17 11:15:59 +0100

    Initial Commit

---
+++ murmr/murmrApp.swift
@@ -0,0 +1,17 @@
+//
+//  murmrApp.swift
+//  murmr
+//
+//  Created by Sam on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct murmrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
